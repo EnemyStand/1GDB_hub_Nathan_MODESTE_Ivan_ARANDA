b31e051370e7d06ab175dad61cad9d43e14b1844
EnemyStand <85677357+EnemyStand@users.noreply.github.com>
2023-03-29 17:16:55 +0200

    Final Version Dungeon

--- 1GDB_Hub_Nathan_Modeste_Ivan_Aranda/Assets/Scripts/CS_PublicAnimator.cs
+++ 1GDB_Hub_Nathan_Modeste_Ivan_Aranda/Assets/Scripts/CS_PublicAnimator.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SC_PublicAnimator : MonoBehaviour
+public class CS_PublicAnimator : MonoBehaviour
 {
     [SerializeField]
     private Animator _Animator = null;
-    private void OnTriggerStay()
+    private void OnTriggerEnter()
     {
         _Animator.Play("Waving");
     }
