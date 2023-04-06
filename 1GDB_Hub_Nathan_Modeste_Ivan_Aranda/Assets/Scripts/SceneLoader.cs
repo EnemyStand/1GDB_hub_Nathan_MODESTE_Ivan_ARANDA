411e6d32f98e78def67b8c63165938c77a9339aa
Modeste Nathan <nathan.mmodeste@gmail.com>
2023-04-06 10:11:24 +0200

    Add SceneLoader

---
+++ 1GDB_Hub_Nathan_Modeste_Ivan_Aranda/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
